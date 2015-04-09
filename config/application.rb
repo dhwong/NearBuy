889a0eac6ef9afe40ae631576d9f7178f916099e
Nigel Gilbert <ngilbert@smu.edu>
2015-04-09 18:42:29 -0500

    Added pacifico, styling to home

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ Bundler.require(*Rails.groups)
 
 module Tutorial
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'fonts')
   end
 end
