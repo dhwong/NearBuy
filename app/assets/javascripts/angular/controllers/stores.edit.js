1775521c433cee88f6f1d832d6603d046da46599
dhwong <dhwong@smu.edu>
2015-04-09 19:58:29 -0500

    pause

--- app/assets/javascripts/angular/controllers/stores.edit.js
+++ app/assets/javascripts/angular/controllers/stores.edit.js
@@ -1,19 +1,18 @@
-APP.controller('StoresListController', ['$scope', 'Restangular', function($scope, Restangular) {
+APP.controller('StoresEditController', ['$scope', 'Restangular', function($scope, Restangular) {
   Restangular.one("stores", $stateParams.id).get().then(function(store) {
     $scope.store = store;
   });
 
   $scope.create = function(store) {
-    Restangular.one("store").post(store).then(function(store) {
+    Restangular.one("stores", $stateParams.id).post(store).then(function(store) {
       $scope.stores.push(store);
     });
   };
 
-  $scope.destroy = function(store) {
-    store.remove().then(function() {
-      _.remove($scope.store, function(w) {
-        return w.id === store.id;
-      });
-    });
-  };
+  function saveStore($scope, Restangular){
+    $scope.store = {};
+    $scope.saveStore=function(){
+      $scope.stores = Restangular.all('data.json/:storeID').post($scope.store);
+    }
+  }
 }]);
