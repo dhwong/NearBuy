7092342825f9bca9263ee22e04b3de120857979e
Nigel Gilbert <ngilbert@smu.edu>
2015-04-19 14:53:47 -0500

    Added preliminary results front/back integration

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'slim-rails'
 gem 'bootstrap-sass'
+gem 'font-awesome-sass'
 
 # Rspecs
 group :development do
--- app/assets/javascripts/angular/controllers/widgets.show.js
+++ app/assets/javascripts/angular/controllers/widgets.show.js
@@ -1,4 +1,4 @@
-APP.controller('WidgetsShowController', ['$scope', 'Restangular', '$stateParams', function($scope, Restangular, $stateParams) {
+app.controller('WidgetsShowController', ['$scope', 'Restangular', '$stateParams', function($scope, Restangular, $stateParams) {
   Restangular.one("widgets", $stateParams.id).get().then(function(widget) {
     $scope.widget = widget;
   });
