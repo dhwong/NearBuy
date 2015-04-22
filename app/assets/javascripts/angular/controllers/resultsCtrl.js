89ef891705745fc25ffbdc0a7834a34e1eba201d
Nigel Gilbert <ngilbert@smu.edu>
2015-04-22 12:18:46 -0500

    Apiary endpoint integration

--- app/assets/javascripts/angular/controllers/resultsCtrl.js
+++ app/assets/javascripts/angular/controllers/resultsCtrl.js
@@ -1,9 +1,24 @@
-app.controller('ResultsListController', ['$scope', 'Restangular', 'Query', function($scope, Restangular, Query) {
-  $scope.Query = Query;
-  Restangular.all("stores").getList().then(function(stores) {
-  		// Get the items list json
-	 		Restangular.all("items").getList().then(function(items) {
-	 			$scope.items = items;
-	 		});
-  });
+app.controller('ResultsListController', ['$scope', 'Restangular', 'Query', 'apiaryFactory',
+	function($scope, Restangular, Query) {
+
+  	$scope.Query = Query;
+  	$scope.results = {};
+
+  	// Get using the existing simplified GET all on the items collection
+	 	Restangular.all("items").getList().then(function(items) {
+	 		$scope.items = items;
+	 	});
+
+	 	// Query apiary results page
+	 	$scope.postSearch = function(query) {
+	 		apiaryFactory.postSearch(query)
+	 		  .success(function() {
+	 		  	$scope.results = Query;
+	 		  })
+	 		  .error(function() {
+	 		  	alert('Apiary post unsuccesful');
+	 		  });
+	 	};
+
+
 }]);
