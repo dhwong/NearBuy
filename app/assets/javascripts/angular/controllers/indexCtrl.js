972b2976a5432cff32d257fe28d5077f2dee0b71
Nigel Gilbert <ngilbert@smu.edu>
2015-04-22 11:08:39 -0500

    Untracked renamed files

--- app/assets/javascripts/angular/controllers/indexCtrl.js
+++
@@ -1,5 +0,0 @@
-app.controller('indexCtrl', ['$scope', 'Restangular', function($scope, Restangular){
-	$scope.create = function(user){
-		Restangular.all("users").post({ "user": user });
-	};
-}]);
--- app/assets/javascripts/angular/controllers/results.list.js
+++
@@ -1,9 +0,0 @@
-app.controller('ResultsListController', ['$scope', 'Restangular', function($scope, Restangular) {
-  // Get the stores
-  Restangular.all("stores").getList().then(function(stores) {
-  		// Get the items list json
-	 		Restangular.all("items").getList().then(function(items) {
-	 			$scope.items = items;
-	 		});
-  });
-}]);
